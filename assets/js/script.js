a93fad645aef29cc85a12ede2cee0c890cc809b6
Daniel Ferilles <dferilles@deloitte.com>
2023-05-25 17:19:52 -0300

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if(hr < 10) hr = '0' + hr;
+        horas.textContent = hr;
+    if(min < 10) min = '0' + min;
+        minutos.textContent = min;
+    if(seg < 10) seg = '0' + seg;
+        segundos.textContent = seg;
+})
